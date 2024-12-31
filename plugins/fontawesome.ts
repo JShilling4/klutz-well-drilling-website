4db9943bad09f93afd0b4ab13f6be3d5cb80e74a
Justin Shilling <loveofmusic25@hotmail.com>
2024-12-31 09:24:24 -0500

    add check icon

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,9 +1,9 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCheck } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faFacebook, faBars);
+library.add(faFacebook, faBars, faCheck);
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
